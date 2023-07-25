5c075ba4758862511dd2d4d33fa67e5e609c30a3
nouman17610 <accanouman@gmail.com>
2023-07-25 04:16:04 -0700

    Correct JS Linters

--- template01.js
+++ template01.js
@@ -1,20 +1,16 @@
-const hamIcon = document.querySelector(".menu-icon");
-const navItems = document.querySelector(".menu-list");
-
-hamIcon.addEventListener("click", () => {
-  hamIcon.classList.toggle("active");
-  navItems.classList.toggle("active");
+const hamIcon = document.querySelector('.menu-icon');
+const navItems = document.querySelector('.menu-list');
+hamIcon.addEventListener('click', () => {
+  hamIcon.classList.toggle('active');
+  navItems.classList.toggle('active');
 });
-
-document.querySelectorAll(".menu-item a").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamIcon.classList.remove("active");
-    navItems.classList.remove("active");
+document.querySelectorAll('.menu-item a').forEach((n) => n.addEventListener('click', () => {
+  hamIcon.classList.remove('active');
+  navItems.classList.remove('active');
   })
 );
-
-const closebutton = document.querySelector(".cross");
-closebutton.addEventListener("click", function () {
-  console.log("button clicked");
-  navItems.classList.add("hide");
+const closebutton = document.querySelector('.cross');
+closebutton.addEventListener("click", function() {
+  console.log('button clicked');
+  navItems.classList.add('hide');
 });
